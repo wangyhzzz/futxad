a4eb3794d4204a97d2c1045e4f97887dcbdc189e
wangyhzzz <wangyhzzzz@gmail.com>
2016-06-05 16:16:12 +0800

    add config

--- futx.cpp
+++ futx.cpp
@@ -1,14 +1,32 @@
 #include <stdio.h>
+/* #define _WIN32 _MSC_VER */
+#define _WIN32_WINNT 0x601
 #include "windows.h"
-#pragma comment(linker,"/subsystem:\"windwos\" /entry:\"mainCRTStartup\"")
+#include "simpleini\SimpleIni.h"
+/* #pragma comment(linker,"/subsystem:\"windwos\" /entry:\"mainCRTStartup\"") */
 
 int main(int argc,_TCHAR* argv[])
 {
-    ShowWindow(GetConsoleWindow(),SW_HIDE); 
+    /* ShowWindow(GetConsoleWindow(),SW_HIDE); */ 
+    /* CSimpleIniA ini; */
+    CSimpleIni ini(false,true,false);
+    ini.SetUnicode();
+    ini.LoadFile("conf.ini");
+    CSimpleIniA::TNamesDepend values;
+    /* const char *p = ini.GetValue("COMMON","DB","default"); */
+    /* ini.SetMultiKey(); */
+    bool ok = ini.GetAllValues("COMMON","NAME",values);
+    if(!ok){
+		MessageBox(NULL,"获取配置文件conf.ini失败...","错误",MB_ICONSTOP);
+        return 1;
+    }
+    /* values.sort(CSimpleIniA::Entry::LoadOrder()); */
     while(1){
         HWND hwnd ;
-        /* (hwnd = FindWindow(("TXGuiFoundation"),("腾讯新闻")))&&SendMessage(hwnd,WM_SYSCOMMAND,SC_CLOSE,NULL); */
-        (hwnd = FindWindow(("TXGuiFoundation"),("腾讯大闽网新闻"))) &&SendMessage(hwnd,WM_SYSCOMMAND,SC_CLOSE,NULL);
+        CSimpleIniA::TNamesDepend::const_iterator i;
+        for(i=values.begin();i!= values.end();i++){
+            (hwnd = FindWindow(("TXGuiFoundation"),(i->pItem)))&&SendMessage(hwnd,WM_SYSCOMMAND,SC_CLOSE,NULL);
+        }
         Sleep(5000);
     }
     return 0;
